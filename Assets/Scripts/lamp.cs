cc5f9f3aec7db7f0cc372614b58437a86e5ea3a2
VimalAdit1 <65787080+VimalAdit1@users.noreply.github.com>
2021-01-26 01:36:26 +0530

    Added props

---
+++ Assets/Scripts/lamp.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lamp : Item
+{
+    bool isOn = false;
+    public GameObject pointLight;
+    public bool burnOnInstance=false;
+    public GameObject particles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isOn = false;
+        pointLight.SetActive(false);
+        particles.SetActive(false);
+        if(burnOnInstance)
+        {
+            Interact();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public override void Interact()
+    {
+        isOn = (isOn == true) ? false : true;
+        StartCoroutine(UpdateState());
+    }
+    IEnumerator UpdateState()
+    {
+        
+        if (isOn)
+        {
+            pointLight.SetActive(true);
+            particles.SetActive(true);
+        }
+        else
+        {
+            pointLight.SetActive(false);
+            particles.SetActive(false);
+        }
+        yield return new WaitForSeconds(.1f);
+    }
+}
